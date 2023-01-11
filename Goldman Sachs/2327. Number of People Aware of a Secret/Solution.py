b25663ec55d8e3c34f475e0b6b545e781be9cd4b
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-01-11 21:13:11 +0530

    Create Solution.py

---
+++ Goldman Sachs/2327. Number of People Aware of a Secret/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+
+        dp = [1] + [0] * (n - 1) 
+
+        mod = 10 ** 9 + 7 
+
+        share = 0 
+
+        for i in range(1, n):
+
+            dp[i] = share = (share + dp[i - delay] - dp[i - forget]) % mod 
+
+        return sum(dp[-forget:]) % mod
