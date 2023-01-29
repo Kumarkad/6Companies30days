b8a5791e770e7716a0e62db85d9cb480b078845a
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-01-29 18:39:04 +0530

    Create Solution.py

---
+++ Amazon/792. Number of Matching Subsequences/Solution.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+class Solution:
+
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+
+        word_dict = defaultdict(list)
+
+        count = 0
+
+        for word in words:
+
+            word_dict[word[0]].append(word)
+
+        for char in s:
+
+            words_expecting_char = word_dict[char] 
+
+            word_dict[char] = [] 
+
+            for word in words_expecting_char:
+
+                if len(word) == 1:
+
+                    count += 1 
+
+                else: 
+
+                    word_dict[word[1]].append(word[1:]) 
+
+        return count
